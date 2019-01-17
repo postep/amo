026187a2b49685ac839addf1acf48284f29e9469
Jakub Postępski <jakub.postepski@gmail.com>
2019-01-17 20:00:58 +0100

    x

--- project2/amo2.m
+++ project2/amo2.m
@@ -1,57 +1,75 @@
-%% generowanie zbioru
-
-a1 = 10;
-b1 = 1;
-a2 = -10;
-b2 = 1;
-
-lambda = 1;
-
-r1 = a1 + (b1-a1)*rand(10, 2);
-y1 = ones(10, 1);
-r2 = a2 + (b2-a2)*rand(10, 2);
-y2 = -ones(10, 1);
- 
-setX = [r1; r2];
-setY = [y1; y2];
-
-fun = @(W) W(1:2)'*W(1:2);
-A = [setX, -ones(20, 1)].*repmat(-setY, [1 3]);
-W = fmincon(fun, ones(3, 1), A, -ones(20, 1));
-
-Class = setX*W(1:2) - W(3) 
+clear variables;
 
 
+%% generowanie danych
+largedata = 0
+if largedata == 0
+	dim = 5;
+	len = 20;
 
-clf;
-hold on;
-plot(r1(:,1), r1(:, 2), 'x')
-plot(r2(:,1), r2(:, 2), 'x')
-hold off;
+	r1 = randn(len/2, dim);
+	r1(:,end) = abs(r1(:, end));
+	y1 = ones(len/2, 1);
+	r2 = randn(len/2, dim);
+	r2(:,end) = -abs(r2(:, end));
+	y2 = -ones(len/2, 1);
 
+	setX = [r1; r2];
+	setY = [y1; y2];
+end
+
+%% obrobka danych saheart
+largedata = 1
+if largedata == 1
+	heartdata = [cell2mat(saheart(:, 1:4)), strcmp(saheart(:, 5), 'Present'), cell2mat(saheart(:, 6:end))];
+	heartdata(:,end) = (heartdata(:, end)-0.5)*2;
+	heartdata(:,5) = (heartdata(:, 5)-0.5)*2;
+
+	dim = size(heartdata);
+	len = dim(1);
+	dim = dim(2)-1;
+
+	setX = heartdata(:, 1:end-1);
+	setY = heartdata(:, end);
+end
+
+
+%% zadnie prymalne
+options = struct('MaxFunctionEvaluations', 10000);
+fun = @(W) W(1:end-1)'*W(1:end-1);
+A = [setX, -ones(len, 1)].*repmat(-setY, [1 dim+1]);
+W = fmincon(fun, ones(dim+1, 1), A, -ones(len, 1), [], [], [], [], [], options);
+
+class_prim = sign(setX*W(1:end-1) - W(end));
 
 
 %% zadanie dualne
-
 H = diag(setY)*setX;
 H = H*H';
 
-f = -ones(20, 1);
-lambda = 0.1;
-
-lb = zeros(20, 1);
-ub = ones(20, 1) /(2*20*lambda);
+f = -ones(len, 1);
+lb = zeros(len, 1);
 
 Aeq = setY';
 beq = 0;
 [C, fval] = quadprog(H, f, [], [], Aeq, beq, lb);
 
-Wd = zeros(1, 2);
+Wd = zeros(1, dim);
 
-for i = 1:20
+for i = 1:len
 	Wd = Wd + C(i) * setY(i) * setX(i, :);
 end;
 
 greatInx = find(C > 1e-5);
 
 bd = Wd*setX(greatInx(1), :)' - setY(greatInx(1));
+class_dual = sign(setX*Wd' - bd);
+
+%% porownanie zbiorow
+
+plot([1:len], [setY, class_prim, class_dual]', 'x');
+legend('dane', 'problem prymalny', 'problem dualny');
+title('Porownanie klasyfikatorow');
+ylim([-1.5 1.5])
+xlim([0, len+1])
+
